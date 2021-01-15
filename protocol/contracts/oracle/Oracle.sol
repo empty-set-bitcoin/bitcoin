fc3d759d076f1d235c3737b88da13ca31fa21741
mehran <mehran.hydary@gmail.com>
2021-01-15 18:43:30 -0500

    Updated return price function in Oracle

--- protocol/contracts/oracle/Oracle.sol
+++ protocol/contracts/oracle/Oracle.sol
@@ -113,7 +113,7 @@ contract Oracle is IOracle {
         _timestamp = blockTimestamp;
         _cumulative = priceCumulative;
 
-        return price;
+        return price.mul(10);
     }
 
     function updateReserve() private returns (uint256) {
